e311c7ab87fb4cc0e54aa41843c800090e44fe87
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-11 15:14:13 -0600

    added html

--- public_html/index.php
+++ public_html/index.php
@@ -2,9 +2,59 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+
+		<!--add CSS file-->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css" />
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
- 		<h1>Hello World!</h1>
+
+		<!-- this is the header -->
+ 		<header>
+			<h1> Hi! This is my page.</h1>
+		</header>
+
+		<!-- this is the main page content -->
+		<main>
+
+			<!-- begin section 1 -->
+			<section>
+				<p>Ut aliqua cupidatat, +1 live-edge messenger bag iceland godard. Cardigan vice ullamco tousled, food truck nihil nostrud asymmetrical vexillologist.Salvia tilde health goth plaid flexitarian pabst. Vegan iceland tousled narwhal chillwave, health goth migas gochujang kombucha yuccie affogato.Dreamcatcher eiusmod distillery, do assumenda kombucha af dolor. Pinterest affogato consectetur crucifix. 3 wolf moon yr pour-over seitan actually, chia velit hammock air plant humblebrag odio narwhal sriracha fashion axe.YOLO tacos sint drinking vinegar poke raw denim locavore, cornhole vape beard accusamus lumbersexual portland nisi.Deep v labore la croix aute, locavore scenester duis.Knausgaard ad health goth, franzen sint umami duis lomo activated charcoal shoreditch plaid irony tbh drinking vinegar.Enamel pin voluptate duis, ramps eiusmod deserunt semiotics shabby chic chambray try-hard fanny pack vexillologist hell of. Kombucha +1 lyft XOXO. Velit lo-fi ethical fam do thundercats. Vaporware master cleanse consequat selfies dolore.</p>
+				<img src="img/506728f353be5a3eae4512beb908d559.jpg" alt="foxy">
+			</section>
+
+			<!-- begin section 2 -->
+			<section>
+				<h2>My Favorite Things</h2>
+				<u1>
+					<li>Food</li>
+					<li>Watching the stars</li>
+					<li>Cupcakes</li>
+					<li>Family</li>
+				</u1>
+			</section>
+
+			<!-- begin section 3 -->
+			<section>
+				<table>
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+					<tr></tr>
+				</table>
+
+			</section>
+		</main>
+
 	</body>
 </html>
