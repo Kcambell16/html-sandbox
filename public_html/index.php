cebc1fea05c7c3daa6f42c61af5dcbb80bd5c589
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-31 10:01:42 -0600

    practice css change background color

--- public_html/index.php
+++ public_html/index.php
@@ -19,7 +19,9 @@
 
 			<!-- begin section 1 -->
 			<section>
+
 				<p>Ut aliqua cupidatat, +1 live-edge messenger bag iceland godard. Cardigan vice ullamco tousled, food truck nihil nostrud asymmetrical vexillologist.Salvia tilde health goth plaid flexitarian pabst. Vegan iceland tousled narwhal chillwave, health goth migas gochujang kombucha yuccie affogato.Dreamcatcher eiusmod distillery, do assumenda kombucha af dolor. Pinterest affogato consectetur crucifix. 3 wolf moon yr pour-over seitan actually, chia velit hammock air plant humblebrag odio narwhal sriracha fashion axe.YOLO tacos sint drinking vinegar poke raw denim locavore, cornhole vape beard accusamus lumbersexual portland nisi.Deep v labore la croix aute, locavore scenester duis.Knausgaard ad health goth, franzen sint umami duis lomo activated charcoal shoreditch plaid irony tbh drinking vinegar.Enamel pin voluptate duis, ramps eiusmod deserunt semiotics shabby chic chambray try-hard fanny pack vexillologist hell of. Kombucha +1 lyft XOXO. Velit lo-fi ethical fam do thundercats. Vaporware master cleanse consequat selfies dolore.</p>
+
 				<img src="img/506728f353be5a3eae4512beb908d559.jpg" alt="foxy">
 			</section>
 
